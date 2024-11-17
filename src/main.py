e82121afe362d4334cc1612024f7d016abfa9458
kalikhalid <naushnikol@gmail.com>
2024-11-17 12:58:45 +0300

    init

---
+++ src/main.py
@@ -0,0 +1,15 @@
+from typing import Union
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse, HTMLResponse
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return RedirectResponse(url="https://t.me/kstudiopaybot")
+
+
+@app.get("/verification.txt")
+def read_item():
+    return HTMLResponse(content="be19a4098d3d1fab16e7602d49296638e57f0411")
