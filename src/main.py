63407fa51bc96e7934059670fc4d0d9072ef4ac2
kalikhalid <naushnikol@gmail.com>
2024-11-20 18:21:23 +0300

    no

--- src/main.py
+++ src/main.py
@@ -4,17 +4,9 @@ from fastapi.responses import RedirectResponse, HTMLResponse
 
 app = FastAPI()
 
-content = """
-<h1>Здравствуйте! Это обращение предназначено сотрудникам модерации tome.ru.</h1>
-<h2>Мой "магазин" работает на платформе Telegram как чат-бот, поэтому прошу проверить его по адресу:</h2>
-<a href="https://t.me/kstudiopaybot">https://t.me/kstudiopaybot</a>
-<h2>Если с магазином что-то не так прошу ответить мне на письмо которое я отправлял вам в поддержку 18.11.2024</h2>
-"""
-
 @app.get("/")
 def read_root():
-    return HTMLResponse(content=content)
-
+    return RedirectResponse(url='https://taplink.cc/kfreelance')
 
 @app.get("/verification.txt")
 def read_item():
